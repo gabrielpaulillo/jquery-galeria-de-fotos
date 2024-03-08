54f71d4adaa0c4ec64d0692641c136d3b7f98dcb
Gabriel Paulillo <gabrielpaulillo@gmail.com>
2024-03-08 09:49:13 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    // Seletor utilizando DOM
+    // document.querySelector('header button');
+
+    // Seletor utilizando jQuery
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none;"></li>');
+        console.log(novoItem);
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('');
+    })
+})
